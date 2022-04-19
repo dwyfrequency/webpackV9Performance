30e2e9d4041a84e16ff101003998d86f81e19dd3
dwyfrequency <jackdwyer@google.com>
2022-04-19 17:30:41 -0400

    Update with Trace code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "../hidden/firebaseConfig";
-import { getPerformance } from "firebase/performance";
+import { getPerformance, trace } from "firebase/performance";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const perf = getPerformance(app);
-console.log(perf.dataCollectionEnabled);
+const t = trace(perf, "CUSTOM_TRACE_NAME");
+t.start();
 
 async function component() {
   const element = document.createElement("div");
@@ -14,7 +15,9 @@ async function component() {
   return element;
 }
 
+t.stop();
 document.body.appendChild(component());
 
+// No change in KB size when adding TRACE
 // WITH NO CONFIG, -rw-r--r--  1 jackdwyer  primarygroup    43K Apr 19 11:44 dist/main.js
 // Run `ls -lh dist/main.js` to get the above
